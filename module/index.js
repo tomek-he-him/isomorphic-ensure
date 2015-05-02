e3bf5d42153db429fd9901af3560f728924babe5
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-05-02 02:37:39 +0200

    Extract list of loaders from the id

--- module/index.js
+++ module/index.js
@@ -1,4 +1,12 @@
 const startsWithDot = /^\./;
+const moduleIdParts = /^((?:[a-z\-]+!)*)(.*)$/;
+  //  ^           From the beginning of the string match:
+  //  ((?:        1.
+  //    [a-z\-]+  A loader name, consisting of lowercase letters and hyphens
+  //    !         followed by a bang
+  //  )*)         as many times as possible.
+  //  (.*)        2. Any sequence of characters
+  //  $           until the end of the string.
 
 export default (settings = {}) => {
   // const {loaders} = settings;
@@ -8,9 +16,11 @@ export default (settings = {}) => {
     const {dirname} = context;
     process.nextTick(() => callback(
       (moduleId) => {
-        const modulePath = (startsWithDot.test(moduleId) ?
-          `${dirname}/${moduleId}` :
-          moduleId
+        const [, loadersList, rawPath] = moduleId.match(moduleIdParts);
+        const loaders = loadersList.split('!').slice(0, -1);
+        const modulePath = (startsWithDot.test(rawPath) ?
+          `${dirname}/${rawPath}` :
+          require.resolve(rawPath)
         );
 
         return require(modulePath);

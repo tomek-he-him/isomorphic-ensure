103b482ba94724eafe155c210ab492cf99110e94
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-05-02 03:03:38 +0200

    Make it work with local text files

--- module/index.js
+++ module/index.js
@@ -1,3 +1,21 @@
+const requireFromString = (source, filename) => {
+  var m = new module.constructor();
+  m._compile(source, filename);
+  return m.exports;
+};
+  // Credits: http://stackoverflow.com/a/17585470/2816199
+
+const loaderContext = {
+  cacheable() {},
+  dependencies() {},
+  async() {throw new Error(
+    'isomorphic-ensure: Async modules are not supported.'
+  );},
+  resolve() {throw new Error(
+    'isomorphic-ensure: Loaders which use `this.resolve` are not supported.'
+  );},
+};
+
 const startsWithDot = /^\./;
 const moduleIdParts = /^((?:[a-z\-]+!)*)(.*)$/;
   //  ^           From the beginning of the string match:
@@ -9,21 +27,32 @@ const moduleIdParts = /^((?:[a-z\-]+!)*)(.*)$/;
   //  $           until the end of the string.
 
 export default (settings = {}) => {
-  // const {loaders} = settings;
-  // const {readFileSync} = require('fs');
+  const {loaders} = settings;
+  const {readFileSync} = require('fs');
 
   return (dependencies, callback, context) => {
     const {dirname} = context;
     process.nextTick(() => callback(
       (moduleId) => {
-        const [, loadersList, rawPath] = moduleId.match(moduleIdParts);
-        const loaders = loadersList.split('!').slice(0, -1);
+        const [, loadersPart, rawPath] = moduleId.match(moduleIdParts);
+        const loadersList = loadersPart.split('!').slice(0, -1);
         const modulePath = (startsWithDot.test(rawPath) ?
           `${dirname}/${rawPath}` :
           require.resolve(rawPath)
         );
 
-        return require(modulePath);
+        return (loadersList.length ?
+          requireFromString(
+            loadersList.reduce(
+              (moduleString, loaderName) => loaders[loaderName]
+                .bind(loaderContext)(moduleString)
+              ,
+              readFileSync(modulePath, {encoding: 'utf8'})
+            ),
+            modulePath.split('/').pop()
+          ) :
+          require(modulePath)
+        );
       }
     ));
   };
